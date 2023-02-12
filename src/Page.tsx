5af63017c8b2f76c0188646cfe1863522d3c0dbe
k_jion <k_jion@example.com>
2023-02-12 15:04:00 +0800

    feat: 设置新的首页

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import NotFound from './components/pages/NotFound';
-import Login from './components/pages/Login';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import App from './App';
+import Login from './components/pages/Login';
+import NotFound from './components/pages/NotFound';
 
 export default () => (
     <Router>
         <Switch>
-            <Route exact path="/" render={() => <Redirect to="/app/dashboard/index" push />} />
+            <Route exact path="/" render={() => <Redirect to="/app/building/list" push />} />
             <Route path="/app" component={App} />
             <Route path="/404" component={NotFound} />
             <Route path="/login" component={Login} />
